f45a14080636e984db520d6bce5fd335cc987950
Quang <34854355+QuangHuynhPK@users.noreply.github.com>
2019-11-21 11:46:44 +0700

    21112019 update in linux

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,8 @@ import service.AuthorServiceImpl;
 public class Main {
     public static void main(String[] args) {
         GenericDao<Integer, Author> author = new AuthorServiceImpl();
+        author.create(1, new Author("Quang", 45535435362l, "abc"));
+        author.create(2, new Author("Duc", 3454534l, "def"));
+        System.out.println(author.getInfo(2).toString());
     }
 }
